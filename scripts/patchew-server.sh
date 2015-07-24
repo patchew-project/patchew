ec9fd001da16f4a8fa470dea450c2c9fc6fc83fb
Fam Zheng <famz@redhat.com>
2015-07-24 10:48:45 +0800

    patchew-server: Import maildir files one by one after mb2md
    It's more robust as an early failure in the large mbox file will not
    stop patchew import command.
    
    Signed-off-by: Fam Zheng <famz@redhat.com>

--- scripts/patchew-server.sh
+++ scripts/patchew-server.sh
@@ -6,13 +6,21 @@ cd ..
 PATCHEW="./patchew --db mongo://$MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT/patchew"
 $PATCHEW server &
 
+import()
+{
+    local OUT=/tmp/patchew-maildir.$$
+    mb2md -s $(realpath $1) -d $OUT
+    find $OUT -exec $PATCHEW import {} \;
+    rm -rf $OUT
+}
+
 while :; do
     for i in 0 1; do
         if curl --time-cond mbox-$i --remote-time \
             -o mbox-$i-new ftp://lists.gnu.org/qemu-devel/$(date "--date=$(date +%Y-%m-15) -$i months" +%Y-%m); then
             if ! cmp mbox-$i-new mbox-$i; then
                 mv mbox-$i-new mbox-$i
-                $PATCHEW import mbox-$i
+                import mbox-$i
             else
                 rm mbox-$i-new
             fi
