48a97c9f82e98cbdb1e9152086c58ed8d90e1465
Fam Zheng <famz@redhat.com>
2015-07-24 15:02:20 +0800

    deploy: Deploy server
    Signed-off-by: Fam Zheng <famz@redhat.com>

--- scripts/deploy-server.sh
+++ scripts/deploy-server.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 set -e
-ip=${1?"usage: $0 <remote address> [<username>]"}
-user=${2:-root}
-remote=$user@$ip
-DOCKER=docker
-
-if [ "$user" != "root" ]; then
-    DOCKER="sudo docker"
-fi
+remote=${1?"usage: $0 <ssh_remote> [docker_command]"}
+DOCKER=${2:-sudo docker}
 
 if ! test -f README.md; then
     echo "Must run from project root (where README.md is)"
     exit 1
 fi
 
-scp -r . $remote:/tmp/patchew-deploy.$$
-ssh $remote "$DOCKER stop patchew-server; $DOCKER rm -f patchew-server; cd /tmp/patchew-deploy.$$ && $DOCKER build -t patchew-server ."
-ssh $remote $DOCKER run --name patchew-mongo -d mongo || true
-ssh $remote $DOCKER run --name patchew-server --link patchew-mongo:mongo -p 8383:8383 -d patchew-server
+rsync -azrC . $remote:/tmp/patchew-deploy
+ssh -t $remote "\
+    mkdir -p /data/db/patchew; \
+    $DOCKER stop patchew-server; \
+    $DOCKER rm -f patchew-server; \
+    $DOCKER rm -f patchew-mongo; \
+    cd /tmp/patchew-deploy; \
+    $DOCKER build -t patchew-server .; \
+    $DOCKER run --name patchew-mongo -v /data/db/patchew:/data/db -d mongo; \
+    sleep 3; \
+    $DOCKER run --name patchew-server --link patchew-mongo:mongo \
+    -p 8383:8383 -d patchew-server"
