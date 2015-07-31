4dfb63c0f3563df324f3aed967cd0878aef03558
Fam Zheng <famz@redhat.com>
2015-07-31 14:21:16 +0800

    patchew: Remove tester
    The testing will be done with Jenkins.
    
    Signed-off-by: Fam Zheng <famz@redhat.com>

--- libpatchew/__init__.py
+++ libpatchew/__init__.py
@@ -2,5 +2,4 @@ from message import Message
 from db import DB, MessageDuplicated
 from search import doctext as search_doctext
 from server import start_server
-from tester import Tester, TestLogger
 from config import load_config, items
