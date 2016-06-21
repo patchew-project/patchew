30b7f8f6fb432f19cc52b904427e84185abb346a
Fam Zheng <famz@redhat.com>
2016-06-21 08:46:48 +0800

    footer: Update documentation
    Signed-off-by: Fam Zheng <famz@redhat.com>

--- mods/footer.py
+++ mods/footer.py
@@ -6,6 +6,17 @@ _default_config = """
 """
 
 class FooterModule(PatchewModule):
+    """
+
+Documentation
+-------------
+
+This is a simple module to inject any HTML code into the page bottom. Can be
+useful to add statistic code, etc..
+
+The config is the raw HTML code to inject.
+
+"""
     name = "footer"
     default_config = _default_config
 
