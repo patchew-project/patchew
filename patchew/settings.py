75d41e90912738d7273ea4b8126182e7400b7a3f
Fam Zheng <famz@redhat.com>
2016-07-12 16:10:46 +0800

    add VERSION file
    Signed-off-by: Fam Zheng <famz@redhat.com>

--- patchew/settings.py
+++ patchew/settings.py
@@ -21,10 +21,10 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 import os
 
-VERSION = "2.0"
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+VERSION = open(os.path.join(BASE_DIR, "VERSION"), "r").read().strip()
 
 MODULE_DIR = os.path.join(BASE_DIR, "mods")
 
