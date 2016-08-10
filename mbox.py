645aea41c360a8cda6a9c2418d507ee2a9f5493a
Fam Zheng <famz@redhat.com>
2016-08-11 07:45:24 +0800

    mbox: Use charset specified in message content type if there is any
    Signed-off-by: Fam Zheng <famz@redhat.com>

--- mbox.py
+++ mbox.py
@@ -146,7 +146,7 @@ class MboxMessage(object):
         payload = self._m.get_payload(decode=not self._m.is_multipart())
         body = ''
         if isinstance(payload, bytes):
-            body = payload.decode("utf-8")
+            body = payload.decode(self._m.get_content_charset() or "utf-8")
         elif isinstance(payload, list):
             for p in payload:
                 if p.get_content_type() == "text/plain":
