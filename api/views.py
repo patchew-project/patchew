d023d5e60a0a15f6f81835f85c9233a96a87ef00
Paolo Bonzini <pbonzini@redhat.com>
2017-12-03 16:07:46 +0100

    unify permissions for project properties
    Properties are handled differently between ListProjectView and
    GetProjectPropertiesView.  ListProjectView was showing them even to
    non-authenticated users, while for GetProjectPropertiesView had no
    allowed_groups so only superuser was allowed.
    
    Project properties _can_ contain sensitive data, so they should be
    hidden from non-maintainers.  However, GetProjectPropertiesView's
    behavior is too restrictive.  After this patch, ListProjectView
    only includes the "properties" key when logged in as a maintainer,
    and GetProjectPropertiesView returns 403 (via a pre-existing,
    but pointless until now, maintained_by check) for non-maintainers.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- api/views.py
+++ api/views.py
@@ -74,21 +74,22 @@ class VersionView(APIView):
     def handle(self, request):
         return settings.VERSION
 
-def prepare_project(p):
-    ret = {
-        "name": p.name,
-        "mailing_list": p.mailing_list,
-        "url": p.url,
-        "git": p.git,
-        "description": p.description,
-        "properties": p.get_properties(),
-    }
-    return ret
-
 class ListProjectView(APIView):
     name = "get-projects"
 
     def handle(self, request, name=None):
+        def prepare_project(p):
+            ret = {
+                "name": p.name,
+                "mailing_list": p.mailing_list,
+                "url": p.url,
+                "git": p.git,
+                "description": p.description,
+            }
+            if p.maintained_by(request.user):
+                ret["properties"] = p.get_properties()
+            return ret
+
         r = [prepare_project(x) for x in Project.objects.all() \
                 if name == None or name == x.name]
         return r
@@ -106,7 +107,7 @@ class AddProjectView(APILoginRequiredView):
                     description=description)
         p.save()
 
-class GetProjectPropertiesView(APILoginRequiredView):
+class GetProjectPropertiesView(APIView):
     name = "get-project-properties"
 
     def handle(self, request, project):
