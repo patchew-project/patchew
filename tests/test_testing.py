223a0c2c2607f1cc1a02360c72a66fa71e547352
Paolo Bonzini <pbonzini@redhat.com>
2018-02-03 22:14:28 -0500

    add REST API support to the testing plugin

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -20,6 +20,8 @@ class TestingTest(PatchewTestCase):
     def setUp(self):
         self.create_superuser()
         self.p = self.add_project("QEMU", "qemu-devel@nongnu.org")
+        self.PROJECT_BASE = '%sprojects/%d/' % (self.REST_BASE, self.p.id)
+
         self.p.set_property("testing.tests.a.timeout", 3600)
         self.p.set_property("testing.tests.a.enabled", True)
         self.p.set_property("testing.tests.a.script", "#!/bin/bash\ntrue")
@@ -37,6 +39,14 @@ class TestingTest(PatchewTestCase):
         self.msg.set_property("git.tag", "dummy tag")
         self.msg.set_property("git.base", "dummy base")
 
+    def msg_testing_done(self, log=None, **report):
+        if not 'passed' in report:
+            report['passed'] = True
+        self.msg.set_property("testing.report.tests", report)
+        if log is not None:
+            self.msg.set_property("testing.log.tests", log)
+        self.msg.set_property("testing.done", True)
+
     def test_basic(self):
         self.api_login()
         td = self.api_call("testing-get",
@@ -46,13 +56,33 @@ class TestingTest(PatchewTestCase):
         self.assertIn("head", td)
 
     def test_done(self):
-        self.msg.set_property("testing.done", True)
+        self.msg_testing_done()
         self.api_login()
         td = self.api_call("testing-get",
                            project="QEMU",
                            tester="dummy tester",
                            capabilities=[])
         self.assertFalse(td)
+
+    def test_rest_basic(self):
+        resp = self.api_client.get(self.PROJECT_BASE + 'series/' + self.msg.message_id + '/')
+        self.assertEquals('testing.tests' in resp.data['results'], False)
+
+    def test_rest_done_success(self):
+        self.msg_testing_done(log='everything good!', passed=True)
+        resp = self.api_client.get(self.PROJECT_BASE + 'series/' + self.msg.message_id + '/')
+        self.assertEquals(resp.data['results']['testing.tests']['status'], 'success')
+        log = self.client.get(resp.data['results']['testing.tests']['log_url'])
+        self.assertEquals(log.status_code, 200)
+        self.assertEquals(log.content, b'everything good!')
+
+    def test_rest_done_failure(self):
+        self.msg_testing_done(log='sorry no good', passed=False)
+        resp = self.api_client.get(self.PROJECT_BASE + 'series/' + self.msg.message_id + '/')
+        self.assertEquals(resp.data['results']['testing.tests']['status'], 'failure')
+        log = self.client.get(resp.data['results']['testing.tests']['log_url'])
+        self.assertEquals(log.status_code, 200)
+        self.assertEquals(log.content, b'sorry no good')
 
 if __name__ == '__main__':
     main()
