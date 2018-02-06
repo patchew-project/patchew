74f741b18e253e042417004d477344450d1a347f
Paolo Bonzini <pbonzini@redhat.com>
2018-02-06 14:31:11 +0100

    use real mbox in test_testing
    This will make it a bit simpler to test the REST API, using the
    same idioms used by other test modules.

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -11,6 +11,7 @@
 import sys
 import os
 import datetime
+sys.path.append(os.path.dirname(__file__))
 from patchewtest import PatchewTestCase, main
 from api.models import Message, Project
 
@@ -18,52 +19,37 @@ class TestingTest(PatchewTestCase):
 
     def setUp(self):
         self.create_superuser()
+        self.p = self.add_project("QEMU", "qemu-devel@nongnu.org")
+        self.p.set_property("testing.tests.a.timeout", 3600)
+        self.p.set_property("testing.tests.a.enabled", True)
+        self.p.set_property("testing.tests.a.script", "#!/bin/bash\ntrue")
+        self.p.set_property("testing.tests.a.requirements", "")
+        self.p.set_property("testing.tests.a.users", "")
+        self.p.set_property("testing.tests.a.tester", "")
+
+        self.cli_login()
+        self.cli_import('0001-simple-patch.mbox.gz')
+        self.cli_logout()
+
+        self.msg = Message.objects.all()[0]
+        self.msg.save()
+        self.msg.set_property("git.repo", "dummy repo")
+        self.msg.set_property("git.tag", "dummy tag")
+        self.msg.set_property("git.base", "dummy base")
 
     def test_basic(self):
-        prj = Project(name="TestProject")
-        prj.save()
-        prj.set_property("testing.tests.a.timeout", 3600)
-        prj.set_property("testing.tests.a.enabled", True)
-        prj.set_property("testing.tests.a.script", "#!/bin/bash\ntrue")
-        prj.set_property("testing.tests.a.requirements", "")
-        prj.set_property("testing.tests.a.users", "")
-        prj.set_property("testing.tests.a.tester", "")
-        msg = Message(project=prj,
-                      date=datetime.datetime.now(),
-                      is_series_head=True,
-                      is_patch=True)
-        msg.save()
-        msg.set_property("git.repo", "dummy repo")
-        msg.set_property("git.tag", "dummy tag")
-        msg.set_property("git.base", "dummy base")
         self.api_login()
         td = self.api_call("testing-get",
-                           project="TestProject",
+                           project="QEMU",
                            tester="dummy tester",
                            capabilities=[])
         self.assertIn("head", td)
 
     def test_done(self):
-        prj = Project(name="TestProject")
-        prj.save()
-        prj.set_property("testing.tests.a.timeout", 3600)
-        prj.set_property("testing.tests.a.enabled", True)
-        prj.set_property("testing.tests.a.script", "#!/bin/bash\ntrue")
-        prj.set_property("testing.tests.a.requirements", "")
-        prj.set_property("testing.tests.a.users", "")
-        prj.set_property("testing.tests.a.tester", "")
-        msg = Message(project=prj,
-                      date=datetime.datetime.now(),
-                      is_series_head=True,
-                      is_patch=True)
-        msg.save()
-        msg.set_property("git.repo", "dummy repo")
-        msg.set_property("git.tag", "dummy tag")
-        msg.set_property("git.base", "dummy base")
-        msg.set_property("testing.done", True)
+        self.msg.set_property("testing.done", True)
         self.api_login()
         td = self.api_call("testing-get",
-                           project="TestProject",
+                           project="QEMU",
                            tester="dummy tester",
                            capabilities=[])
         self.assertFalse(td)
