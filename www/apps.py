42aef3b18930aa850078e0eaf2d5cbe97a7c4875
Caio Carrara <ccarrara@redhat.com>
2018-11-28 19:56:16 -0200

    Apply PEP8 to www apps and urls modules
    The adjustments in this change were basically indentation and line
    breaks.
    
    Signed-off-by: Caio Carrara <ccarrara@redhat.com>
    Message-Id: <20181128215617.9284-2-ccarrara@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- www/apps.py
+++ www/apps.py
@@ -8,8 +8,6 @@
 # This work is licensed under the MIT License.  Please see the LICENSE file or
 # http://opensource.org/licenses/MIT.
 
-
-
 from django.apps import AppConfig
 
 
--- www/urls.py
+++ www/urls.py
@@ -17,23 +17,23 @@ urlpatterns = []
 dispatch_module_hook("www_url_hook", urlpatterns=urlpatterns)
 
 urlpatterns += [
-        url(
-            '^login/$',
-            auth_views.login,
-            {'template_name': 'login.html'},
-            name="login"
-        ),
-        url(
-            '^logout/$',
-            auth_views.logout,
-            name="logout"
-        ),
-        url(r"^search$", views.view_search, name="search"),
-        url(r"^search-help$", views.view_search_help, name="search_help"),
-        url(r"^(?P<project>[^/]*)/$", views.view_series_list, name="series_list"),
-        url(r"^(?P<project>[^/]*)/info$", views.view_project_detail, name="project_detail"),
-        url(r"^(?P<project>[^/]*)/(?P<message_id>[^/]*)/$", views.view_series_detail, name="series_detail"),
-        url(r"^(?P<project>[^/]*)/(?P<thread_id>[^/]*)/(?P<message_id>[^/]*)/$", views.view_series_message, name="series_message"),
-        url(r"^(?P<project>[^/]*)/(?P<message_id>[^/]*)/mbox$", views.view_mbox, name="mbox"),
-        url(r"^$", views.view_project_list, name="project_list"),
-        ]
+    url(
+        '^login/$',
+        auth_views.login,
+        {'template_name': 'login.html'},
+        name="login"
+    ),
+    url(
+        '^logout/$',
+        auth_views.logout,
+        name="logout"
+    ),
+    url(r"^search$", views.view_search, name="search"),
+    url(r"^search-help$", views.view_search_help, name="search_help"),
+    url(r"^(?P<project>[^/]*)/$", views.view_series_list, name="series_list"),
+    url(r"^(?P<project>[^/]*)/info$", views.view_project_detail, name="project_detail"),
+    url(r"^(?P<project>[^/]*)/(?P<message_id>[^/]*)/$", views.view_series_detail, name="series_detail"),
+    url(r"^(?P<project>[^/]*)/(?P<thread_id>[^/]*)/(?P<message_id>[^/]*)/$", views.view_series_message, name="series_message"),
+    url(r"^(?P<project>[^/]*)/(?P<message_id>[^/]*)/mbox$", views.view_mbox, name="mbox"),
+    url(r"^$", views.view_project_list, name="project_list"),
+]
