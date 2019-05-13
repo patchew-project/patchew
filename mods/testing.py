ee6f0954d63a127b739abe7d80a7ef60e593f99a
Paolo Bonzini <pbonzini@redhat.com>
2019-05-13 22:42:37 +0200

    git: automatically derive the URL for result PUT
    Similar to commit 4efd0d21145a973f3321e9ec93700fb061b289f5 (git: allow not
    passing the URL to applier-report) but for the REST API.

--- mods/testing.py
+++ mods/testing.py
@@ -20,6 +20,7 @@ import time
 import math
 from api.views import APILoginRequiredView
 from api.models import Message, MessageResult, Project, ProjectResult, Result
+import api.rest
 from api.rest import PluginMethodField, TestPermission, reverse_detail
 from api.search import SearchEngine
 from event import emit_event, declare_event, register_handler
@@ -52,7 +53,7 @@ class TestingLogViewer(LogView):
         return _instance.get_testing_result(obj, testing_name)
 
 
-class ResultDataSerializer(serializers.Serializer):
+class ResultDataSerializer(api.rest.ResultDataSerializer):
     # TODO: is_timeout should be present iff the result is a failure
     is_timeout = BooleanField(required=False)
     head = CharField()
