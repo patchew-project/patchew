39cb1891c1cbbd257877d590a848e1c310f13c42
Fam Zheng <fam@euphon.net>
2020-10-14 11:20:03 +0100

    settings: Set X_FRAME_OPTIONS
    The default was changed from SAMEORIGIN to DENY since django 3.0.
    
    The colorbox uses iframe to display contents, keep the old behavior to
    avoid browser rejection.

--- patchew/settings.py
+++ patchew/settings.py
@@ -34,6 +34,8 @@ MODULE_DIR = os.path.join(BASE_DIR, "mods")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "@f-l5@70om7o(7rda^oxd$f#60g3jy#&m^p7z@vkf+&$*@%!^o"
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = []
 
 # Application definition
